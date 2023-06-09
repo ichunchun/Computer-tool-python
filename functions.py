582d5004c5441bca24065523fa95eace90449a5e
Computer_Pc <School_Pc>
2023-06-09 17:18:08 +0800

    一些更改

--- functions.py
+++ functions.py
@@ -97,26 +97,61 @@ def auto_modify():
     ModifyIP(NetCard, Net_IP, Net_MASK, Net_gateway, Net_DNS1, Net_DNS2)
 
 
-    def set_computer_name(name):
-        key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName'
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
-        winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
-        winreg.CloseKey(key)
+    # def set_computer_name(name):
+    #     key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName'
+    #     key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+    #     winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
+    #     winreg.CloseKey(key)
 
-        key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName'
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
-        winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
-        winreg.CloseKey(key)
+    #     key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName'
+    #     key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+    #     winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
+    #     winreg.CloseKey(key)
 
-        key_path = r'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters'
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
-        winreg.SetValueEx(key, 'Hostname', 0, winreg.REG_SZ, name)
-        winreg.SetValueEx(key, 'NV Hostname', 0, winreg.REG_SZ, name)
-        winreg.CloseKey(key)
+    #     key_path = r'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters'
+    #     key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+    #     winreg.SetValueEx(key, 'Hostname', 0, winreg.REG_SZ, name)
+    #     winreg.SetValueEx(key, 'NV Hostname', 0, winreg.REG_SZ, name)
+    #     winreg.CloseKey(key)
 
 
 
-    set_computer_name(PC_name)
+    # set_computer_name(PC_name)
+
+    def modify_name(pc_name):
+
+        # 定义要修改的计算机名称
+        new_computer_name = pc_name
+
+        # 修改注册表中的计算机名称
+        key_path = r"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName"
+        value_name = "ComputerName"
+        value_type = winreg.REG_SZ
+
+        try:
+            # 打开注册表键
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_SET_VALUE)
+
+            # 写入新的计算机名称
+            winreg.SetValueEx(key, value_name, 0, value_type, new_computer_name)
+
+            # 关闭注册表键
+            winreg.CloseKey(key)
+            # print("成功修改注册表中的计算机名称")
+        except Exception as e:
+            print("修改注册表中的计算机名称时出现错误:", str(e))
+            # pass
+
+        # 执行系统命令来修改计算机名称
+        command = f"wmic computersystem where name='%COMPUTERNAME%' call rename name='{new_computer_name}'"
+        try:
+            subprocess.check_output(command, shell=True)
+            # print("成功修改计算机名称")
+        except subprocess.CalledProcessError as e:
+            print("修改计算机名称时出现错误:", str(e))
+            # pass
+
+    modify_name(PC_name)
 
     temp_number = int(start)
 
--- main.py
+++ main.py
@@ -15,17 +15,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # self.setAttribute(Qt.WA_TranslucentBackground) #圆角背景透明
 
         Net_IP,Net_gateway,PC_name = calc_ip()
-        print(PC_name)
-
+        # print(PC_name)
+        print(get_computer_name())
         self.label.setText(get_computer_name())
         self.label_2.setText(get_ip_addresses())
         self.pushButton_3.setText("Change to\n\n{PC_name}\n\n{Net_IP}".format(PC_name=PC_name, Net_IP=Net_IP))
         self.pushButton_close.clicked.connect(lambda: self.close())
+        self.pushButton_3.clicked.connect(lambda: change(self))
         self.pushButton_4.clicked.connect(lambda: save_mac(self))
 
         def save_mac(self):
             get_text()
-            self.pushButton_4.setText("已保存\n\n请勿重复点击")
+            self.pushButton_4.setText("Save Already\n\n请勿重复点击")
+
+        def change(self):
+            auto_modify()
+            self.pushButton_3.setText("Changed Already\n\n{PC_name}\n\n{Net_IP}".format(PC_name=PC_name, Net_IP=Net_IP))
         
 
     def mousePressEvent(self, event):
