66855795f531d760fde976804528abc9a4318ca5
Computer_Pc <School_Pc>
2023-06-09 13:45:10 +0800

    完善基础功能

---
+++ functions.py
@@ -0,0 +1,116 @@
+import wmi
+import subprocess
+import configparser
+import winreg
+
+cfp = configparser.ConfigParser()
+cfp.read("Config.ini")
+
+
+
+def get_mac_address():
+    c = wmi.WMI()
+    network_adapters = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
+    mac_addresses = [adapter.MACAddress for adapter in network_adapters]
+    return mac_addresses[0]
+
+def get_computer_name():
+    c = wmi.WMI()
+    computer_system = c.Win32_ComputerSystem()[0]
+    computer_name = computer_system.Name
+    return computer_name
+
+def get_ip_addresses():
+    c = wmi.WMI()
+    network_adapters = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
+    ip_addresses = []
+    for adapter in network_adapters:
+        ip_addresses.extend(adapter.IPAddress)
+    return ip_addresses[0]
+
+
+def get_text():
+    mac_addresses = get_mac_address().replace(":", "-")
+    computer_name = get_computer_name()
+    ip_addresses = get_ip_addresses()
+    with open('ttt.txt', 'a') as f:
+        f.write(computer_name+"   "+ip_addresses+"    "+mac_addresses+"   "+computer_name + '\n')
+
+
+
+def auto_modify():
+    room = cfp.get("Main","room")
+    offset = cfp.get("Main","offset")
+    start = cfp.get("Main","start")
+    row = cfp.get("Main","row").upper()
+
+    # print(ord(row)-64)
+    number = ord(row)-65
+
+    IP_Pre3 = "192.168.33"
+
+    if room == '201':
+        IP_Pre3 = "192.168.24"
+    elif room == '216':
+        IP_Pre3 = "192.168.26"
+    elif room == '501':
+        IP_Pre3 = "192.168.35"
+    elif room == '308':
+        IP_Pre3 = "192.168.33"
+
+    PC_name = room + row + start
+    IP_last = int(start) + int(offset)*number + 100
+
+    # print(PC_name) 
+
+    NetCard = "以太网"
+    Net_IP = IP_Pre3 + "." + str(IP_last)
+    Net_MASK = "255.255.255.0"
+    Net_gateway = IP_Pre3 + "."+"1"
+    Net_DNS1 = "114.114.114.114"
+    Net_DNS2 = "8.8.8.8"
+
+    # print(Net_IP+"   "+Net_MASK+"   "+Net_gateway+"   "+Net_DNS1+"   "+Net_DNS2)
+
+
+    def ModifyIP(NetCard, Net_IP, Net_MASK, Net_gateway, DNS1, DNS2):
+
+        cmd = f'netsh interface ip set address name={NetCard} static {Net_IP} {Net_MASK} {Net_gateway} 1'
+        dns_cmd = f'netsh interface ip set dns name={NetCard} static {DNS1} primary'
+        dns_backup_cmd = f'netsh interface ip add dns name={NetCard} addr={DNS2} index=2'
+        
+        subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen(dns_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen(dns_backup_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    ModifyIP(NetCard, Net_IP, Net_MASK, Net_gateway, Net_DNS1, Net_DNS2)
+
+
+    def set_computer_name(name):
+        key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName'
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
+        winreg.CloseKey(key)
+
+        key_path = r'SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName'
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key, '', 0, winreg.REG_SZ, name)
+        winreg.CloseKey(key)
+
+        key_path = r'SYSTEM\CurrentControlSet\Services\Tcpip\Parameters'
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key, 'Hostname', 0, winreg.REG_SZ, name)
+        winreg.SetValueEx(key, 'NV Hostname', 0, winreg.REG_SZ, name)
+        winreg.CloseKey(key)
+
+
+
+    set_computer_name(PC_name)
+
+    temp_number = int(start)
+
+    temp_number = temp_number + 1
+
+    cfp.set("Main", "start", str(temp_number))
+
+    cfp.write(open("Config.ini", "w"))
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PySide6.QtCore import Qt
 from computer_ui import Ui_MainWindow
+from functions import *
 
+cfp = configparser.ConfigParser()
+cfp.read("Config.ini")
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -11,7 +14,39 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowFlag(Qt.FramelessWindowHint) #无边框
         # self.setAttribute(Qt.WA_TranslucentBackground) #圆角背景透明
 
+        room = cfp.get("Main","room")
+        offset = cfp.get("Main","offset")
+        start = cfp.get("Main","start")
+        row = cfp.get("Main","row").upper()
+
+        number = ord(row)-65
+
+        IP_Pre3 = "192.168.33"
+
+        if room == '201':
+            IP_Pre3 = "192.168.24"
+        elif room == '216':
+            IP_Pre3 = "192.168.26"
+        elif room == '501':
+            IP_Pre3 = "192.168.35"
+        elif room == '308':
+            IP_Pre3 = "192.168.33"
+
+        IP_last = int(start) + int(offset)*number + 100
+
+        PC_name = room + row + start
+        Net_IP = IP_Pre3 + "." + str(IP_last)
+
+        self.label.setText(get_computer_name())
+        self.label_2.setText(get_ip_addresses())
+        self.pushButton_3.setText("Change to\n\n{PC_name}\n\n{Net_IP}".format(PC_name=PC_name, Net_IP=Net_IP))
         self.pushButton_close.clicked.connect(lambda: self.close())
+        self.pushButton_4.clicked.connect(lambda: save_mac(self))
+
+        def save_mac(self):
+            get_text()
+            self.pushButton_4.setText("已保存\n\n请勿重复点击")
+        
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -22,6 +57,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if event.buttons() == Qt.LeftButton:
             self.move(event.globalPos() - self.dragPosition)
             event.accept()
+
 
 
 if __name__ == "__main__":
